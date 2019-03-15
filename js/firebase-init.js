7b4659e02bb625744304f7a229704e81bf33ff5e
adam0thman <adam0thman@gmail.com>
2019-03-15 08:30:55 +0800

    Create firebase-init.js

---
+++ js/firebase-init.js
@@ -0,0 +1,44 @@
+
+<script src="https://www.gstatic.com/firebasejs/5.9.0/firebase.js"></script>
+<script src="https://www.gstatic.com/firebasejs/5.9.0/firebase-storage.js"></script>
+<script src="https://www.gstatic.com/firebasejs/5.9.0/firebase-firestore.js"></script>
+<script src="https://www.gstatic.com/firebasejs/5.9.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/5.9.0/firebase-auth.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDGsek6bhfTV0UvMQO3ZkfLI27rh5Db0DI",
+    authDomain: "imhere-5e2dc.firebaseapp.com",
+    databaseURL: "https://imhere-5e2dc.firebaseio.com",
+    projectId: "imhere-5e2dc",
+    storageBucket: "imhere-5e2dc.appspot.com",
+    messagingSenderId: "932669852417"
+  };
+  
+  //Initialize main firebase app
+  firebase.initializeApp(config);
+  
+  // Initialize Cloud Firestore through Firebase
+  var db = firebase.firestore();
+    
+  // Disable deprecated features
+  db.settings({
+    timestampsInSnapshots: true
+  });
+    
+  // Variables
+  //var pageURL = window.location.protocol+"//"+window.location.host;
+  //var pageURL = 'https://www.streamshop.io';
+  var pageLogin = "/login.php";
+  var pageMain = "/main.php?v=main";
+  var pageAdmin = "/admin.php?v=admin";
+    
+  // Function for Logging out
+  function callLogout() {
+    firebase.auth().signOut().then(function() {
+      window.location.replace(pageURL+pageMain);
+    }).catch(function(error) {
+      console.log(error);
+    });
+  }
+</script>
